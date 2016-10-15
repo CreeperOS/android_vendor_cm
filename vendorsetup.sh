25c8eccfa9a51e4f81d009607e9bdefe64665581
Rafael PacMe <rafy.pachecomendez@gmail.com>
2016-10-15 06:59:33 -0300

    add Creeper Things to Hoopy Integration

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,4 @@
-add_lunch_combo creeper_y6-userdebug
-add_lunch_combo creeper_golden-userdebug
+for combo in $(curl -s https://raw.githubusercontent.com/CreeperOS-Devices/hoopy/master/creeper-build-targets | sed -e 's/#.*$//' | grep OS-lp | awk '{printf "creeper_%s-%s\n", $1, $2}')
+do
+    add_lunch_combo $combo
+done
